711d227c19a124663e86e6e35b2ce82a8d888269
Leandro <leandrowebmaster@gmail.com>
2025-09-29 10:23:36 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -2530,7 +2530,7 @@ return (
 	right: 0,
 	top: -60,
 	zIndex: 1,
-	//alignItems: 'center',
+	padding: 10,
 }`, `{ backgroundColor: "$var_all.colors.smoke" }`],
 
             functions:[()=>{}],            childrenItems:[
