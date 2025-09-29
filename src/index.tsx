ab5bd8b9678178194ff00fcce7525e9068a7842f
Leandro <leandrowebmaster@gmail.com>
2025-09-29 10:56:03 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -2775,7 +2775,11 @@ borderRadius: 10,
 
             styles:[`{ 
 	width: "fit-content",
- minWidth: 120, height: 30, backgroundColor: "$var_all.colors.primary", borderRadius: 5, alignItems: "center", justifyContent: "center" }`],
+		minWidth: 120, height: 30, backgroundColor: "$var_all.colors.primary", 		borderRadius: 5, 
+	alignItems: "center", 
+	justifyContent: "center",
+	margim: 10, 
+}`],
 
             functions:[async (...args) =>
  functions.funcGroup({ args, pass:{
