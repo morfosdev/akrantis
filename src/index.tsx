924c5fa18f684ec2027a065904f7a2d0c54b334c
Leandro <leandrowebmaster@gmail.com>
2025-10-06 13:35:38 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -1299,7 +1299,7 @@ width={14}     height={12}     fill="red"     viewBox="0 0 14 12"     {...props}
           ],
 
           children: [
-            `Ir p/ APP`
+            `Ir p/ Industrias`
           ],
 
           args,
