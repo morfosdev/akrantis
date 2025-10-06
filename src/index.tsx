2d0ccd158ee9055a095f9abd7974d4851092ab00
Leandro <leandrowebmaster@gmail.com>
2025-10-06 11:23:52 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -4704,8 +4704,8 @@ xmlns="http://www.w3.org/2000/svg"
  } 
 , 'sc': { 
 'A0': { 'forms': { 'iptsChanges': {
-	userEmail: "adm@teste.com",
-	userPassword: "1234"
+	userEmail: "admin",
+	userPassword: "admin"
 } } 
  } 
 , 
