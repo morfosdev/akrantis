7f97b8d2c35e75cef158daf588a2ad27ef9142d2
Renan <72417996+Rehito0@users.noreply.github.com>
2025-10-10 09:33:00 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -2114,14 +2114,9 @@ justifyContent: "center"
           (...args:any) => <Elements.DynView pass={{
             elementsProperties:['{}'],
 
-            styles:[
-              {
-                backgroundColor: 'white',
-                justifyContent: 'center',
-                minHeight: 22,
-                width: "100%",
-              }
-              ],
+            styles:[`{
+	backgroundColor: 'transparent',
+}`],
 
             functions:[()=>{}],            childrenItems:[
         (...args:any) => <Elements.Text pass={{
